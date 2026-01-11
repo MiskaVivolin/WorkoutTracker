0656a48448aa20a76328664236b1dae122e6af02
Miska Vivolin <miska.vivolin@gmail.com>
2026-01-11 14:21:39 +0200

    added deleteAccount UI placeholder

--- frontend/src/screens/SettingScreen.tsx
+++ frontend/src/screens/SettingScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform, View, StyleSheet } from 'react-native';
+import { Platform, View, Text, StyleSheet } from 'react-native';
 import { WorkoutListScreenProps } from '../types/screenProps';
 import TabBar from '../components/navigation/TabBar';
 import NavBar from '../components/navigation/NavBar';
@@ -7,6 +7,7 @@ import { Themes } from '../../assets/styles/Themes'
 import { useTheme } from '../context/ThemeContext';
 import ThemeSwitcher from '../components/ThemeSwitcher';
 import Logo from '../components/Logo';
+import Button from '../components/Button';
 
 const WorkoutListScreen: React.FC<WorkoutListScreenProps> = ({ navigation }) => {
   const { theme } = useTheme();
@@ -23,6 +24,16 @@ const WorkoutListScreen: React.FC<WorkoutListScreenProps> = ({ navigation }) => 
       ) : (
         <View style={{ flex: 1 }}>
           <NavBar navigation={navigation}/>
+          <View style={styles.deleteAccountContainer}>
+            <Text style={[styles.title, {color: Themes[theme].defaultText}]}>Delete your Account</Text>
+            <View style={styles.buttonContainer}>
+              <Button
+                buttonStyle={{marginRight: Platform.OS === 'android' || Platform.OS === 'ios' ? 0 : 50, width: 90}}
+                title="Delete" 
+                onPress={() => console.log("placeholder")}
+                />
+            </View>
+          </View>
           <ThemeSwitcher/>
         </View>
       )}
@@ -36,6 +47,22 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  deleteAccountContainer: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '80%',
+    justifyContent: Platform.OS === 'android' || Platform.OS === 'ios' ? "space-evenly" : "center",
+  }, 
+  title: {
+    marginBottom: Platform.OS === 'android' || Platform.OS === 'ios' ? 20 : 50, 
+    fontFamily: 'MerriweatherSans',
+    fontSize: Platform.OS === 'android' || Platform.OS === 'ios' ? 18 : 20, 
+  }
 });
 
 export default WorkoutListScreen;
